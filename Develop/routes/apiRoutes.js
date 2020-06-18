4909115f0fd0525350bba1e98e24df12ac79f3dd
Ryan Simpkins <rythsimpkins@yahoo.com>
2020-06-17 22:54:56 -0700

    :(

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,22 +2,22 @@
 var db = require("../db/db.json");
 
 const fakeDB = [];
-// JSON.pars and JSON.stringfy
+
 module.exports = function(app) {
   
   app.get("/api/notes", function(req, res) {
-    console.log(fakeDB);
-    res.json(fakeDB);
+    console.log(db);
+    res.json(db);
   });
 
   app.post("/api/notes", function(req, res) {
 
-    fakeDB.push(req.body);
+    db.push(req.body);
     res.json(req.body);
   })
 
   app.delete("/api/notes/:id", function(req, res) {
-  // filter method
+  
     res.json({ ok: true });
   });
 };
